96e2dfb8d789d082d69cc059f34a3f27b97cdfd3
Guillermo García <guillermieres97@gmail.com>
2022-10-07 01:03:15 +0200

    responsive home

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <script>
 import { RouterView } from "vue-router";
 import TheHeader from "./components/TheHeader.vue";
-import TheHeaderCreate from "./components/TheHeaderCreate.vue";
+
 import TheFooter from "./components/TheFooter.vue";
-import TheFooterCreate from "./components/TheFooterCreate.vue";
 
 export default {
   name: "App",
   components: {
     RouterView,
     TheHeader,
-    TheHeaderCreate,
+
     TheFooter,
-    TheFooterCreate,
   },
 };
 </script>
@@ -25,9 +23,6 @@ export default {
       this.$route.path != '/'
     "
   />
-  <TheHeaderCreate
-    v-if="this.$route.path === '/create' || this.$route.path === '/edit'"
-  />
 
   <RouterView />
 
@@ -37,9 +32,6 @@ export default {
       this.$route.path != '/edit' &&
       this.$route.path != '/'
     "
-  />
-  <TheFooterCreate
-    v-if="this.$route.path === '/create' || this.$route.path === '/edit'"
   />
 </template>
 
--- src/components/TheCarousel.vue
+++ src/components/TheCarousel.vue
@@ -64,82 +64,150 @@
 
 <style scoped>
 @import "../assets/base.css";
-/*  */
+
+/* carousel */
 #carouselExampleControls {
-  width: 100%;
-  height: 300px;
+  width: 100vw;
+  height: 40vh;
   color: white;
 }
 .carousel-item {
-  height: 300px;
-  padding: 25px 90px;
+  height: 40vh;
+  padding: 5vh 10vw;
   box-shadow: 0 0 10px rgba(130, 130, 130, 0.775) inset;
 }
 .flex {
   display: flex;
   justify-content: space-around;
   align-items: center;
-  margin-top: 20px;
+  margin-top: 5vh;
 }
-/*  */
+
+/* green item */
 #corporation {
   background-color: var(--lightgreen-palette);
 }
 #corporation img {
-  margin-top: 15px;
+  margin-top: 2.5vh;
   width: 15%;
   border-radius: 10px;
 }
 #corporation h1 {
-  font-size: 45px;
-  text-shadow: 1px 1px;
+  margin-top: 2.5vh;
+  font-size: 3vw;
 }
 #corporation span {
   color: var(--green-palette);
 }
 #corporation p {
-  margin-left: 5px;
-  font-size: 22px;
+  margin-left: 0.5vw;
+  font-size: 1.5vw;
 }
-/*  */
+
+/* orange item */
 #motivation {
   background-color: rgb(246, 173, 17);
 }
 #motivation img {
   width: 12%;
-  margin-top: 20px;
+  margin-top: 2.5vh;
   border-radius: 10px;
 }
 #motivation h1 {
-  margin-top: 40px;
-  font-size: 45px;
-  text-shadow: 1px 1px;
+  margin-top: 2.5vh;
+  font-size: 3vw;
 }
 #motivation span {
   color: var(--orange-palette);
 }
 #motivation p {
-  margin-left: 7px;
-  font-size: 22px;
+  margin-left: 0.6vw;
+  font-size: 1.5vw;
 }
-/*  */
+
+/* red item */
 #product {
   background-color: #a83e55;
 }
 #product img {
   width: 15%;
-  margin-top: 28px;
+  margin-top: 2.5vh;
   border-radius: 10px;
 }
 #product h1 {
-  margin-top: 50px;
-  font-size: 45px;
-  text-shadow: 1px 1px;
+  margin-top: 2.5vh;
+  font-size: 3vw;
 }
 #product span {
   color: var(--pink-palette);
 }
 #product p {
-  font-size: 22px;
+  font-size: 1.5vw;
+}
+
+/* media queries 1000 800 650 500px */
+@media (max-width: 1000px) {
+  #carouselExampleControls {
+    height: 33vh;
+  }
+  .carousel-item {
+    height: 33vh;
+  }
+  h1 {
+    font-size: 4vw !important;
+  }
+  p {
+    font-size: 2vw !important;
+  }
+}
+@media (max-width: 800px) {
+  #carouselExampleControls {
+    height: 28vh;
+  }
+  .carousel-item {
+    height: 28vh;
+  }
+  h1 {
+    font-size: 4vw !important;
+  }
+  p {
+    font-size: 2vw !important;
+  }
+}
+
+@media (max-width: 650px) {
+  #carouselExampleControls {
+    height: 28vh;
+  }
+  .carousel-item {
+    height: 28vh;
+  }
+  h1 {
+    font-size: 4vw !important;
+  }
+  p {
+    font-size: 2vw !important;
+  }
+}
+@media (max-width: 500px) {
+  #carouselExampleControls {
+    height: 24vh;
+  }
+  .carousel-item {
+    height: 24vh;
+  }
+  img {
+    margin-top: 1vh;
+  }
+  h1 {
+    margin-top: 1vh;
+    font-size: 5vw !important;
+  }
+  p {
+    font-size: 3vw !important;
+  }
+  button {
+    display: none;
+  }
 }
 </style>
